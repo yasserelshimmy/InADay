cb81b164a6186accdebb454c43bcf3ab97abfaef
Yasser Elshimmy <yasbizness@gmail.com>
2021-03-21 13:50:54 +0200

    only 1

--- InADay/InADay/settings.py
+++ InADay/InADay/settings.py
@@ -21,7 +21,7 @@ USER_AGENT = 'Osama Stress test'
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 36
+CONCURRENT_REQUESTS = 1
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
